1c5d997058d8dd13edded689dd3a2b4aa3a3a4f5
Abdul Mannan <102965402+abdulmannan991@users.noreply.github.com>
2024-11-05 20:25:13 +0500

    improving

--- app/Components/Services.tsx
+++ app/Components/Services.tsx
@@ -13,27 +13,26 @@ export default function Services(){
    Flutter
 </h1>
 
-<p className="text-xs text-white leading-relaxed font-bold group-hover:text-black">
+<p className="text-xs text-white leading-relaxed md:text-sm  group-hover:text-black">
     Flutter allows for fast development of natively compiled applications for mobile, web, and desktop from a single codebase, providing rich, expressive UI capabilities. With its hot-reload feature, developers can quickly iterate on changes, making it ideal for rapid prototyping. Additionally, Flutter’s extensive widget library enables high-performance custom UI elements, ensuring consistent appearance across devices.
   </p>
 
 </div>
 
-<div className="sm:flex sm:justify-end bg-gray-800  w-64 rounded-lg mb-6 p-4 flex flex-wrap  ml-8 hover:bg-teal-300 transition-all hover:scale-110 duration-700 group">
-<h1 className=" mb-4 text-white font-serif -tracking-tight text-xl font-bold   md:text-4xl    w-auto flex justify-center group-hover:text-black">
+<div className="sm:flex  bg-gray-800 justify-center w-64 rounded-lg mb-6 p-4 flex flex-wrap  ml-8 hover:bg-teal-300 transition-all hover:scale-110 duration-700 group">
+<h1 className=" mb-4 text-white  font-serif -tracking-tight text-xl font-bold   md:text-4xl    w-auto flex justify-center group-hover:text-black">
    Web Developer
 </h1>
-
-<p className=" text-xs text-white leading-relaxed group-hover:text-black">
+<p className=" text-xs text-white leading-relaxed md:text-sm  group-hover:text-black">
     Web development involves creating dynamic and interactive websites using HTML for structure, CSS for styling, and JavaScript for functionality. HTML provides the foundational layout, while CSS enhances visual appeal and responsiveness across devices. JavaScript enables developers to add interactivity, create animations, and handle user input, resulting in engaging web experiences that effectively meet users needs.
   </p>
 </div>
 
-<div className="sm:flex sm:justify-start bg-gray-800  w-64 rounded-lg mb-6 p-4 flex flex-wrap  ml-8 hover:bg-teal-300 transition-all hover:scale-110 duration-700 group">
+<div className="sm:flex sm:justify-center bg-gray-800  w-64 rounded-lg mb-6 p-4 flex flex-wrap  ml-8 hover:bg-teal-300 transition-all hover:scale-110 duration-700 group">
 <h1 className=" mb-4 text-white font-serif -tracking-tight text-xl font-bold   md:text-4xl    w-auto flex justify-center group-hover:text-black">
    TypeScript
 </h1>
-<p className="text-xs text-white leading-relaxed group-hover:text-black">
+<p className="text-xs text-white leading-relaxed md:text-sm  group-hover:text-black">
     TypeScript is a superset of JavaScript that adds static typing, enabling developers to catch errors early during development. With features like interfaces and generics, TypeScript enhances code maintainability and readability. It integrates seamlessly with existing JavaScript code and offers powerful tooling support, making it ideal for large-scale applications where robustness and scalability are crucial.
   </p>
 </div>
@@ -44,7 +43,7 @@ export default function Services(){
    C++ 
 </h1>
 
-<p className="text-xs text-white leading-relaxed group-hover:text-black">
+<p className="text-xs text-white leading-relaxed md:text-sm   group-hover:text-black">
     C++ is a powerful, high-performance programming language that supports object-oriented programming, enabling developers to create complex systems and applications. With its rich library support and low-level memory manipulation capabilities, C++ is widely used in system programming, game development, and applications requiring real-time processing. Its versatility makes it an essential language for both beginners and experienced developers.
   </p>
 </div>
